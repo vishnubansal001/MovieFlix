7639bb118bd9a17acbea4f9cb0a07301973c0398
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2024-07-21 18:15:54 +0530

    added the routes for the updating and deleting the movies

--- src/main/java/com/movieflix/movies/Controllers/MovieController.java
+++ src/main/java/com/movieflix/movies/Controllers/MovieController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/movie")
